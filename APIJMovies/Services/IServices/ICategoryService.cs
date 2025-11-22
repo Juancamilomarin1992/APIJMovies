d2209ca33c4f12b2717a24de162db3c5cce88834
Juancamilomarin1992 <juanmarin306639@correo.itm.edu.co>
2025-11-21 22:15:34 -0500

    IServices

---
+++ APIJMovies/Services/IServices/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using APIJMovies.DAL.Models;
+
+namespace APIJMovies.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); 
+        Task<Category> GetCategoryAsync(int id); 
+        Task<bool> CategoryExistsByIdAsync(int id); 
+        Task<bool> CategoryExistsByNameAsync(string name); 
+        Task<bool> CreateCategoryAsync(Category category); 
+        Task<bool> UpdateCategoryAsync(Category category); 
+        Task<bool> DeleteCategoryAsync(int id); 
+    }
+}
